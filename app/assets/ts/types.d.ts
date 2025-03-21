3a4ab40ba55d0cd7e77f86c66047fb077681cf8b
Jairus Joer <jairus.joer@hero-software.de>
2025-03-21 21:13:16 +0100

    Fix type issues

--- app/assets/ts/types.d.ts
+++ app/assets/ts/types.d.ts
@@ -1,6 +1,38 @@
 export interface Metadata {
-  title: string;
   description?: string;
   image?: string;
   schema?: Record<string, any>;
+  title: string;
+}
+
+export interface Collaborator {
+  displayName: string;
+  profilePhoto: string;
+  profileURL: string;
+}
+
+export interface CollectionItem {
+  attachments?: any[];
+  collaborators?: Collaborator[];
+  company?: string;
+  degree?: string;
+  description?: string;
+  event?: string;
+  handle?: string;
+  heading?: string;
+  id: string;
+  location?: string;
+  platform?: string;
+  presenter?: string;
+  publisher?: string;
+  school?: string;
+  title?: string;
+  type: 'workExperience' | 'project' | 'award' | 'talk' | 'writing' | 'education';
+  url?: string;
+  year?: string;
+}
+
+export interface Collection {
+  items: CollectionItem[];
+  name: string;
 }
