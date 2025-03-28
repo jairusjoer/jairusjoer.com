37c883cd99e0fcdd1308af4f517402c742cea16d
Jairus Joer <jairus.joer@hero-software.de>
2025-03-28 12:13:19 +0100

    Install lodash and improve navigation

--- src/composables/useCollectionURL.ts
+++
@@ -1,5 +0,0 @@
-import type { DataEntryMap } from 'astro:content';
-
-export const useCollectionURL = (string: keyof DataEntryMap) => {
-  return string.replace(/([a-z0-9])([A-Z])/g, '$1-$2').toLowerCase();
-};
