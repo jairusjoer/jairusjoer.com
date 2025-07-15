b92d3da1b9586e8959cd0eeace2ed6bcaab42046
Jairus Joer <jairus.joer@hero-software.de>
2025-07-15 12:31:03 +0200

    Add profile page schema

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,6 @@
-export const defaultMetadata = {
+import type { Metadata } from './types';
+
+export const defaultMetadata: Metadata = {
   title: 'Jairus Joer',
   description:
     'I am a Full Stack Developer and Design Systems Expert from Germany, creating scalable architectures and cohesive design systems that enhance product quality.',
