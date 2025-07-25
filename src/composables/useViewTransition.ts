71c49fd098ab56706f7cd5fdf91ad5c00097b878
Jairus Joer <jairus.joer@hero-software.de>
2025-07-25 18:09:30 +0200

    Further reduce complexity

--- src/composables/useViewTransition.ts
+++
@@ -1,7 +0,0 @@
-export const useViewTransition = (method: Function) => {
-  if (!document.startViewTransition) {
-    return method();
-  }
-
-  return document.startViewTransition(() => method());
-};
