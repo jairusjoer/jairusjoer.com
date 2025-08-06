121cc4f99dc7db1074f96d9ebdfee1c338b1b59f
Jairus Joer <jairus.joer@hero-software.de>
2025-08-06 21:15:58 +0200

    Rename components and reorganize structure

--- src/composables/useComponents.ts
+++
@@ -1,7 +0,0 @@
-const components = import.meta.glob<{ default: object }>('/src/components/*.astro', { eager: true });
-
-export const useComponents = () => {
-  return Object.fromEntries(
-    Object.entries(components).map(([path, component]) => [path.split('/').pop()?.split('.')[0], component.default]),
-  );
-};
--- src/utils/useExternalizedLinks.ts
+++ src/utils/useExternalizedLinks.ts
@@ -1,5 +1,6 @@
 export const useExternalizedLinks = () => {
   const links = document.querySelectorAll<HTMLAnchorElement>('.prose a');
+
   links.forEach((link) => {
     if (!link.href.startsWith(location.origin)) {
       link.setAttribute('target', '_blank');
