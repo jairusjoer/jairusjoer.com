c4d08d3276856c706f9fb51c9ba8adee5b0b0a49
Jairus Joer <jairus.joer@hero-software.de>
2025-09-16 22:49:14 +0200

    refactor: update package.json format script, remove duplicate blog content

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,22 +2,6 @@
 import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
-// Blog
-const blog = defineCollection({
-  loader: glob({
-    base: `./src/content/blog`,
-    pattern: '**/*.{md,mdx}',
-  }),
-  schema: ({ image }) =>
-    z.object({
-      draft: z.boolean().optional(),
-      image: image().optional(),
-      title: z.string(),
-      description: z.string().optional(),
-      date: z.date(),
-    }),
-});
-
 // Pages
 const pages = defineCollection({
   loader: glob({
@@ -34,6 +18,5 @@ const pages = defineCollection({
 });
 
 export const collections = {
-  blog,
   pages,
 };
