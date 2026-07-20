ec409da7d9ce7c369e7d08a58444eeea4dfc0231
Jairus 'JJ' Joer <jairus.joer@bwi.de>
2026-07-20 19:45:27 +0200

    feat: Update theme colors and styles for improved aesthetics and accessibility

--- astro.config.ts
+++ astro.config.ts
@@ -2,27 +2,18 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import vue from '@astrojs/vue';
 import tailwindcss from '@tailwindcss/vite';
-import { defineConfig, fontProviders } from 'astro/config';
+import { defineConfig } from 'astro/config';
 import { site } from './src/config';
 
 // https://astro.build/config
 export default defineConfig({
-  fonts: [
-    {
-      provider: fontProviders.fontsource(),
-      name: 'Inter',
-      weights: [400, 500],
-      cssVariable: '--font-inter',
-    },
-  ],
-
   integrations: [mdx(), sitemap(), vue()],
 
   markdown: {
     shikiConfig: {
       themes: {
-        dark: 'github-dark-high-contrast',
-        light: 'github-light-high-contrast',
+        dark: 'catppuccin-mocha',
+        light: 'catppuccin-latte',
       },
     },
   },
--- src/stores/content/themeStore.ts
+++ src/stores/content/themeStore.ts
@@ -1,6 +1,15 @@
 import { map } from 'nanostores';
 
+function matchMode(light: string, dark: string) {
+  if (import.meta.env.SSR) return light;
+
+  const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  if (isDarkMode) return dark;
+
+  return light;
+}
+
 export const $theme = map({
-  color: '#10b981',
+  color: matchMode('#8839ef', '#cba6f7'),
   tokens: 11,
 });
