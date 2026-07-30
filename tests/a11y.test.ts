be8c7363733ff50360329e20b0ed3ec9dcb60144
Jairus 'JJ' Joer <jairus.joer@bwi.de>
2026-07-30 12:48:28 +0200

    feat: Add dark mode a11y checks

--- tests/a11y.test.ts
+++ tests/a11y.test.ts
@@ -4,17 +4,32 @@ import { AxeBuilder } from '@axe-core/playwright';
 import { expect, test } from '@playwright/test';
 import { site } from '../src/config';
 
-test.describe('', () => {
-  const sitemapPath = resolve(process.cwd(), 'dist/sitemap-0.xml');
-  const sitemapContent = readFileSync(sitemapPath, 'utf-8');
+const sitemapPath = resolve(process.cwd(), 'dist/sitemap-0.xml');
+const sitemapContent = readFileSync(sitemapPath, 'utf-8');
 
-  const urlRegex = /<loc>(https?:\/\/[^<]+)<\/loc>/g;
-  const urls = [...sitemapContent.matchAll(urlRegex)].map((url) => {
-    return url[1].replace(site.url, '');
-  });
+const urlRegex = /<loc>(https?:\/\/[^<]+)<\/loc>/g;
+const urls = [...sitemapContent.matchAll(urlRegex)].map((url) => {
+  return url[1].replace(site.url, '');
+});
 
+test.describe('light', () => {
   for (const url of urls) {
     test(url, async ({ page }) => {
+      await page.goto(url);
+
+      const results = await new AxeBuilder({ page })
+        .withTags(['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa', 'wcag22aa'])
+        .analyze();
+
+      expect(results.violations.length, JSON.stringify(results.violations, null, 2)).toBe(0);
+    });
+  }
+});
+
+test.describe('dark', () => {
+  for (const url of urls) {
+    test(url, async ({ page }) => {
+      await page.emulateMedia({ colorScheme: 'dark' });
       await page.goto(url);
 
       const results = await new AxeBuilder({ page })
